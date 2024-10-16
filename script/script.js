0f71ae1deac605761c5376e4573eddef4185cccd
rayelus5 <rayelusanom828@gmail.com>
2024-10-16 16:57:41 +0200

    HUGE UPDATE

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
 cboxmain = document.getElementById("cboxmain");
+cboxcuenta = document.getElementById("cboxcuenta");
 
 cboxmain.setAttribute("height", "1200px");
+cboxcuenta.setAttribute("height", "800px");
+
